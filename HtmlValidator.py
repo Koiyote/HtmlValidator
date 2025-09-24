ebc39995d2bf494440922a1df618965b35b3f492
Koiyote <cclin36@wgu.edu>
2025-09-24 07:54:08 -0400

    First Commit

---
+++ HtmlValidator.py
@@ -0,0 +1,41 @@
+#stack to check against
+class Stack:
+	def __init__main:
+		bucket[]
+	
+	def push(value):
+		bucket[len(bucket)-1)] = value
+		
+	def pop:
+		return bucket[len(bucket)-1]
+		
+	def peek:
+		return bucket[len(bucket)-1]
+		
+#input source
+html = "<html><body></html>"		
+    #after first < keep going till you reach >
+    #add that tag to the stack with push()
+#iterate through the stack
+     #pop each element off the stack and check if the 
+     
+class Dictionary:
+	def __init__(self):
+		self.Keys[110]
+		self.Values[110]
+		
+	def insert(value):
+		self.Keys[value % 110]
+		
+	def read(lookup):
+		return value = Values[Keys[lookup % 110]]
+		
+	def modify(lookup):
+		position = Keys[lookup%110]]
+		temp = Values[position] 
+		temp + 1
+		Values[position] = temp
+#if comparisons to see what is not valid html
+
+
+
